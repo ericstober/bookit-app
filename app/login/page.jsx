b1b8cd16a00cf2a262b47d4e574d9f352963c9a9
ericstober <erictstober@gmail.com>
2024-10-14 16:18:23 -0500

    add formState for login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect } from "react";
+import { useFormState } from "react-dom";
+import createSession from "@/app/actions/createSession";
 
 const LoginPage = () => {
+  const [state, formAction] = useFormState(createSession, {});
+
   return (
     <div className='flex items-center justify-center'>
       <div className='bg-white shadow-lg rounded-lg p-6 w-full max-w-sm mt-20'>
-        <form>
+        <form action={formAction}>
           <h2 className='text-2xl font-bold text-center text-gray-800 mb-6'>Login</h2>
 
           <div className='mb-4'>
