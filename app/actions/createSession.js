ae24c135a549bc4706182bc8ec5adf6465b58841
ericstober <erictstober@gmail.com>
2024-10-14 16:40:52 -0500

    add user auth via appwrite

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,14 +1,43 @@
 "use server";
 
-async function createSession(previous, formData) {
+import { createAdminClient } from "@/config/appwrite";
+import { cookies } from "next/headers";
+
+async function createSession(previousState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
 
   if (!email || !password) {
-    return { error: "Please fill out all fields" };
+    return {
+      error: "Please fill out all fields",
+    };
   }
 
-  console.log(email, password);
+  // Get Account Instance
+  const { account } = await createAdminClient();
+
+  try {
+    // Generate Session
+    const session = await account.createEmailPasswordSession(email, password);
+
+    // Create Cookie
+    cookies().set("appwrite-session", session.secret, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      expires: new Date(session.expire),
+      path: "/",
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.log("Authentication Error: ", error);
+    return {
+      error: "Invalid Credentials",
+    };
+  }
 }
 
 export default createSession;
