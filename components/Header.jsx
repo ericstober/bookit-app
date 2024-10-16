f37d0d151ebf341d617a58a141aaa3058f42be93
ericstober <erictstober@gmail.com>
2024-10-16 15:41:33 -0500

    added handleLogout

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,26 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import logo from "@/assets/images/logo.svg";
 import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from "react-icons/fa";
+import destroySession from "@/app/actions/destroySession";
+import { toast } from "react-toastify";
 
 const Header = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const { success, error } = await destroySession();
+
+    if (success) {
+      router.push("/login");
+    } else {
+      toast.error(error);
+    }
+  };
+
   return (
     <header className='bg-gray-100'>
       <nav className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
@@ -53,9 +70,9 @@ const Header = () => {
                 <FaBuilding className='inline mr-1' /> My Rooms
               </Link>
 
-              <Link href='/login' className='mx-3 text-gray-800 hover:text-gray-600'>
+              <button onClick={handleLogout} className='mx-3 text-gray-800 hover:text-gray-600'>
                 <FaSignOutAlt className='inline mr-1' /> Sign Out
-              </Link>
+              </button>
             </div>
           </div>
         </div>
