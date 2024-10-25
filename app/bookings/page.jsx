04e069af6f87a373c71b681ab5c7ba4e3e7ea983
ericstober <erictstober@gmail.com>
2024-10-25 11:36:28 -0500

    add BookedRoomCard component

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,5 +1,19 @@
-const BookingsPage = () => {
-  return <>bookings</>;
+import Heading from "@/components/Heading";
+import BookedRoomCard from "@/components/BookedRoomCard";
+import getMyBookings from "../actions/getMyBookings";
+
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
+
+  return (
+    <>
+      {bookings.length === 0 ? (
+        <p className='text-gray-600 mt-4'>You have no bookings</p>
+      ) : (
+        bookings.map((booking) => <BookedRoomCard key={booking.$id} booking={booking} />)
+      )}
+    </>
+  );
 };
 
 export default BookingsPage;
